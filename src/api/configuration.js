b2e38ade92e73d1ac88aaad03ba622ed8b341529
lebronejames2323 <ginovillar02@gmail.com>
2025-05-16 03:37:48 +0800

    apicyberdriver.styxhydra

--- src/api/configuration.js
+++ src/api/configuration.js
@@ -1,4 +1,4 @@
-export const url = "http://127.0.0.1:8000/api";
-export const imageUrl1 = "http://127.0.0.1:8000/storage/uploads/products";
-export const imageUrl2 = "http://127.0.0.1:8000/storage/uploads/categories";
-export const imageUrl3 = "http://127.0.0.1:8000/storage/uploads/reviews";
+export const url = "http://apicyberdriver.styxhydra.com/api";
+export const imageUrl1 = "http://apicyberdriver.styxhydra.com/storage/uploads/products";
+export const imageUrl2 = "http://apicyberdriver.styxhydra.com/storage/uploads/categories";
+export const imageUrl3 = "http://apicyberdriver.styxhydra.com/storage/uploads/reviews";
